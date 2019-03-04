b67cdaff65e5d9ea8dbf7c1e16cf1cdfbe9f3563
Serhat Cillidag <serhat@intenseye.com>
2019-03-04 10:19:00 +0300

    Updates

---
+++ precache-manifest.f7c11ce89aae7f8cd7459fcace1c5ba5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3de0d1bbb293d3b072c8",
+    "url": "/intenseye-boun-workshop/static/css/main.2d55f339.chunk.css"
+  },
+  {
+    "revision": "3de0d1bbb293d3b072c8",
+    "url": "/intenseye-boun-workshop/static/js/main.3de0d1bb.chunk.js"
+  },
+  {
+    "revision": "a82a035a9f83c1354041",
+    "url": "/intenseye-boun-workshop/static/js/runtime~main.a82a035a.js"
+  },
+  {
+    "revision": "f8dad12f9ba570a7951f",
+    "url": "/intenseye-boun-workshop/static/js/2.f8dad12f.chunk.js"
+  },
+  {
+    "revision": "61ac3b4525c038d03da1a7bc551d96e4",
+    "url": "/intenseye-boun-workshop/index.html"
+  }
+];
